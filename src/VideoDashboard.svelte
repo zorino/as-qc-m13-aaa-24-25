40244e4bbe092439482a692990ad5086349a3a63
Maxime Deraspe <mderaspe@imarcom.net>
2025-01-26 18:38:49 -0500

    feat: Update seekVideo function to use start and end parameters

--- src/VideoDashboard.svelte
+++ src/VideoDashboard.svelte
@@ -2,10 +2,12 @@
 export let videoId;
 export let persons = [];
 
-const seekVideo = (time, duration) => {
+const seekVideo = (start, end) => {
+
   const iframe = document.querySelector('iframe');
+
   iframe.contentWindow.postMessage(
-    JSON.stringify({ event: 'command', func: 'seekTo', args: [time, true] }),
+    JSON.stringify({ event: 'command', func: 'seekTo', args: [start, true] }),
     '*'
   );
 
@@ -15,6 +17,8 @@ const seekVideo = (time, duration) => {
     '*'
   );
 
+  const duration = ((end-start)+3)*1000;
+
   // Stop the video after 60 seconds
   setTimeout(() => {
     iframe.contentWindow.postMessage(
@@ -22,18 +26,16 @@ const seekVideo = (time, duration) => {
       '*'
     );
   }, duration);
+
 };
 </script>
 
 <div class="container mx-auto p-4">
   <h1 class="text-2xl font-bold mb-4">Video Dashboard</h1>
-  <div class="mb-4">
-    <a href="/hockey-games" class="text-blue-500 hover:underline">Go to Hockey Games</a>
-  </div>
   <div class="video mb-4">
     <iframe
-      width="50%"
-      height="360"
+      width="960"
+      height="480"
       src={`https://www.youtube.com/embed/${videoId}?enablejsapi=1`}
       frameborder="0"
       allow="autoplay; encrypted-media"
@@ -43,12 +45,13 @@ const seekVideo = (time, duration) => {
   {#each persons as person}
     <div class="mb-4">
       <h2 class="text-xl font-semibold">{person.name}</h2>
+
       <ul>
         {#each person.sequences as sequence}
           <li class="mb-2">
             <button
               class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
-              on:click={() => seekVideo(sequence.start, 6000)}
+              on:click={() => seekVideo(sequence.start, sequence.end)}
               >
               {sequence.label}
             </button>
