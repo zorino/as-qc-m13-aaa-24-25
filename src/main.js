156cc340ffade09f988353838562e34d56c6ed52
Maxime Deraspe <mderaspe@imarcom.net>
2025-01-30 20:45:17 -0500

    WIP upgrade svelte 5

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import App from './App.svelte';
+import { mount } from 'svelte';
+// import App from './App.svelte';
+const app = mount(App, { target: document.body });
 
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
-});
+// const app = new App({
+// 	target: document.body,
+// 	props: {
+// 		name: 'world'
+// 	}
+// });
 
 export default app;
